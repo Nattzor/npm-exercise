e229aa0664200d2bf2146fff9122d1620019a135
Markus <markus.mork@iths.se>
2022-11-11 16:09:10 +0100

    Init functionality

--- date-and-time-helpers/src/timeHelper.mjs
+++ date-and-time-helpers/src/timeHelper.mjs
@@ -1,4 +1,22 @@
-const timeHelper = ({startDate, endDate}) => {
-	return 'November'
+import dayjs from 'dayjs'
+// import 'dayjs/locale/en-gb.js'
+import minMax from 'dayjs/plugin/minMax.js'
+dayjs.extend(minMax)
+dayjs.locale('en-gb') // en-gb starts week on monday, en does not
+const yyyymmdd = 'YYYY-MM-DD' // useful for simple .format(yyyymmdd)
+
+const timeHelper = (startDate, endDate) => {
+	const endOfMonth = dayjs(endDate).endOf('month')
+	try{
+		if(dayjs(startDate).$D > 1 || dayjs(endDate).$D !== endOfMonth.$D){
+			throw 'No valid period'
+		}
+		const startMonthName = dayjs(startDate).format('MMMM')
+		const endMonthName = dayjs(endDate).format('MMMM')
+		return startMonthName === endMonthName ? startMonthName : 'No valid period'
+	}catch(err){
+		return err
+	}
 }
 
+console.log(timeHelper('2021-07-01', '2021-07-31'))
