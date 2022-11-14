c97884d4879f65a7a95ea2f1d29443d4d680c90c
Markus <markus.mork@iths.se>
2022-11-14 09:44:22 +0100

    Continue work

--- date-and-time-helpers/src/timeHelper.mjs
+++ date-and-time-helpers/src/timeHelper.mjs
@@ -6,11 +6,16 @@ dayjs.locale('en-gb') // en-gb starts week on monday, en does not
 const yyyymmdd = 'YYYY-MM-DD' // useful for simple .format(yyyymmdd)
 
 const timeHelper = (startDate, endDate) => {
+	// console.log('startDate ', 'dayjs(startDate))
+	const startOfYear = dayjs(startDate).startOf('year')
+	const endOfYear = dayjs(endDate).endOf('year')
+	console.log('start and end of year ', startOfYear, endOfYear)
 	const endOfMonth = dayjs(endDate).endOf('month')
 	try{
 		if(dayjs(startDate).$D > 1 || dayjs(endDate).$D !== endOfMonth.$D){
 			throw 'No valid period'
 		}
+
 		const startMonthName = dayjs(startDate).format('MMMM')
 		const endMonthName = dayjs(endDate).format('MMMM')
 		return startMonthName === endMonthName ? startMonthName : 'No valid period'
@@ -19,4 +24,5 @@ const timeHelper = (startDate, endDate) => {
 	}
 }
 
-console.log(timeHelper('2021-07-01', '2021-07-31'))
+
+console.log(timeHelper('2021-01-01', '2021-12-31'))
