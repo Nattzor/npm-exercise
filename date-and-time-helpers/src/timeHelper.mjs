9aec46abad4729024f8aa4d425e7eb6e5c22961d
Markus <markus.mork@iths.se>
2022-11-14 11:19:49 +0100

    Fix function for publishing

--- date-and-time-helpers/src/timeHelper.mjs
+++ date-and-time-helpers/src/timeHelper.mjs
@@ -6,23 +6,23 @@ dayjs.locale('en-gb') // en-gb starts week on monday, en does not
 const yyyymmdd = 'YYYY-MM-DD' // useful for simple .format(yyyymmdd)
 
 const timeHelper = (startDate, endDate) => {
-	// console.log('startDate ', 'dayjs(startDate))
-	const startOfYear = dayjs(startDate).startOf('year')
-	const endOfYear = dayjs(endDate).endOf('year')
-	console.log('start and end of year ', startOfYear, endOfYear)
-	const endOfMonth = dayjs(endDate).endOf('month')
+	const startOfYear = dayjs(startDate).startOf('year').format(yyyymmdd)
+	const endOfYear = dayjs(endDate).endOf('year').format(yyyymmdd)
+	const startOfMonth = dayjs(startDate).startOf('month').format(yyyymmdd)
+	const endOfMonth = dayjs(endDate).endOf('month').format(yyyymmdd)
+	console.log('start ', dayjs(startDate).format(yyyymmdd), startOfYear)
+	console.log('end ', dayjs(endDate).format(yyyymmdd), endOfYear)
 	try{
-		if(dayjs(startDate).$D > 1 || dayjs(endDate).$D !== endOfMonth.$D){
-			throw 'No valid period'
+		if(dayjs(startDate).format(yyyymmdd) === startOfYear && dayjs(endDate).format(yyyymmdd) === endOfYear){
+			return dayjs(startDate).format('YYYY')
 		}
-
-		const startMonthName = dayjs(startDate).format('MMMM')
-		const endMonthName = dayjs(endDate).format('MMMM')
-		return startMonthName === endMonthName ? startMonthName : 'No valid period'
+		if(dayjs(startDate).format(yyyymmdd) === startOfMonth && dayjs(endDate).format(yyyymmdd) === endOfMonth){
+			return dayjs(startDate).format('MMMM')
+		}
+		throw 'No valid period'
 	}catch(err){
 		return err
 	}
 }
 
-
-console.log(timeHelper('2021-01-01', '2021-12-31'))
+console.log(timeHelper('2021-01-01', '2021-12-28'))
